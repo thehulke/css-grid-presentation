a544b042267de6fbb76a8d6a74c6a6ae42cc7255
Lior Gerstein <lior.gerstein@mac-liorge.local>
2017-09-14 11:02:45 +0300

    got up untill the grid slide

--- presentation/index.js
+++ presentation/index.js
@@ -3,16 +3,13 @@ import React from "react";
 
 // Import Spectacle Core tags
 import {
-  BlockQuote,
-  Cite,
-  Deck,
-  Heading,
-  ListItem,
-  List,
-  Image,
-  Quote,
-  Slide,
-  Text
+    Deck,
+    Heading,
+    Markdown,
+    Link,
+    Slide,
+    Text,
+    Appear
 } from "spectacle";
 
 import CodeSlide from "spectacle-code-slide";
@@ -24,69 +21,118 @@ import preloader from "spectacle/lib/utils/preloader";
 import createTheme from "spectacle/lib/themes/default";
 
 // Require CSS
+require('../assets/font-awesome/css/font-awesome.min.css');
 require("normalize.css");
 require("spectacle/lib/themes/default/index.css");
 
 // images
 const images = {
-  confused: require("../assets/kat.png"),
-  babelLogo: require("../assets/babel.png"),
-  gulpLogo: require("../assets/gulp.png"),
-  gruntLogo: require("../assets/grunt.png"),
-  npmLogo: require("../assets/npm.png"),
-  yarnLogo: require("../assets/yarn.png"),
-  webpackLogo: require("../assets/webpack.png")
+    layout     : require("../assets/layout.png"),
+    confused   : require("../assets/kat.png"),
+    babelLogo  : require("../assets/babel.png"),
+    gulpLogo   : require("../assets/gulp.png"),
+    gruntLogo  : require("../assets/grunt.png"),
+    npmLogo    : require("../assets/npm.png"),
+    yarnLogo   : require("../assets/yarn.png"),
+    webpackLogo: require("../assets/webpack.png")
 };
 // gifs
-const gifs = {
-  enough: require("../assets/enough.gif"),
-  what: require("../assets/what.gif")
+const gifs   = {
+    enough: require("../assets/enough.gif"),
+    what  : require("../assets/what.gif"),
+    cube  : require("../assets/cube.gif")
 };
 
 preloader(images);
 preloader(gifs);
 
 // code for slide about const
-const constCode = 
-`const foo = 'bar';
-foo = 'baz';`;
+const htmlCode =
+          `<div class="wrapper">
+  <nav class="navbar">
+    Navbar
+  </nav>
+  
+  <header class="header">
+    Header
+  </header>
+  
+  <div class="main">
+    <aside class="main-sidebar">SideBar</aside>
+    <div class="main-content">Main Content</div>
+  </div>
+  
+  <footer class="footer">
+    Footer
+  </footer>
+</div>`;
 
 const theme = createTheme({
-  blue: "#0084ff",
-  yellow: "#EEB902",
-  grey: "#474647",
-  white: "white"
+    blue  : "#3679B5",
+    yellow: "#EEB902",
+    grey  : "#474647",
+    white : "white"
 }, {
-    primary: "Montserrat",
-    secondary: "Helvetica"
-  });
+    primary  : "Helvetica Neue",
+    secondary: "Montserrat",
+    awesome  : "FontAwesome"
+});
 
 export default class Presentation extends React.Component {
-  render() {
-    return (
-      <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
+    render() {
+        return (
+            <Deck transition={["fade", "fade"]} transitionDuration={500} theme={theme}>
 
-        <Slide transition={["fade"]} bgColor="blue">
-          <Heading size={2} fit caps lineHeight={1} textColor="yellow">
-            Liorush
-          </Heading>
-          <Text margin="10px 0 0" textColor="white" size={1} fit bold>
-            Matoki amiti :)
-          </Text>
-        </Slide>
+                <Slide transisiton={["fade"]} bgColor="blue">
+                    <Heading size={2} fit caps textColor="white">Basic Layout</Heading>
 
-        <CodeSlide
-          transition={["fade"]}
-          lang="js"
-          bgColor="grey"
-          code={constCode}
-          ranges={[
-            { loc: [0, 1] },
-            { loc: [1, 2], note: "Uncaught TypeError: Assignment to constant variable." }
-          ]}
-        />
+                    <Link bold href="https://codepen.io/liorgrs/full/MyGEQa?editors=1100" target="blank"
+                          textColor="yellow">
+                        <i className="fa fa-codepen"/> View on codepen
+                    </Link>
+                </Slide>
 
-      </Deck>
-    );
-  }
+                <Slide transisiton={["fade", "slide"]} bgImage={gifs.cube}/>
+
+                <CodeSlide
+                    textSize={30}
+                    transition={["fade"]}
+                    lang="html"
+                    bgColor="grey"
+                    code={htmlCode}
+                    ranges={[
+                        {loc: [0, 18], title: "flexBox"},
+                        {loc: [0, 1], note: ".wrapper { display: flex; flex-direction: column; }"},
+                        {loc: [9, 13], note: ".main { display: flex; }"},
+                        {loc: [10, 11], note: ".main-sidebar { max-width: 300px; min-width: 200px }"},
+                        {loc: [11, 12], note: ".main-content { flex-grow: 1; }"}
+                    ]}
+                />
+
+                <Slide transisiton={["fade", "slide"]} bgColor="white">
+
+                    <Heading size={2} caps textColor="blue">conclusion</Heading>
+
+                    <Appear fid="1">
+                        <Text>Every opposite dimension requires to add an extra .wrapper</Text>
+                    </Appear>
+
+                        {/*<Markdown>{*/}
+                            {/*'Every opposite dimension requires to add an extra `.wrapper` '*/}
+                        {/*}</Markdown>*/}
+
+                </Slide>
+
+                <Slide transition={["fade"]} bgColor="blue">
+                    <Heading size={1} caps lineHeight={1} textColor="yellow" textFont="secondary">
+                        #
+                    </Heading>
+                    <Text margin="10px 0 0" textColor="white" size={1} fit>
+                        {"{ display : grid }"}
+                    </Text>
+                </Slide>
+
+            </Deck>
+        );
+    }
 }
